a87a5e65ea1ebea01d9d7dda52527e93edc706b9
Jonathan Roy <jonaisroy@gmail.com>
2024-03-11 11:19:15 +0300

    Get player 1's choice

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 "use strict";
 
-console.log("Hello World!");
+// Get comoputer choice
+function getComputerChoice() {
+  // choices
+  const choices = ["rock", "paper", "scissors"];
+
+  // get random indexes
+  const randomIndex = Math.floor(Math.random() * choices.length);
+
+  // return choice corresponding to random index
+  return choices[randomIndex];
+}
