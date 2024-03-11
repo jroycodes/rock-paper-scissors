d51da5da447126e74bffd6d874897b252671b868
Jonathan Roy <jonaisroy@gmail.com>
2024-03-11 11:20:17 +0300

    Get and validate user input

--- script.js
+++ script.js
@@ -11,3 +11,20 @@ function getComputerChoice() {
   // return choice corresponding to random index
   return choices[randomIndex];
 }
+
+// Get user choice
+function getUserChoice() {
+    // prompt for user input
+    let userChoice = prompt("Rock, Paper, Scissors?");
+  
+    // Validate user input
+    userChoice = userChoice.toLowerCase();
+    if (!["rock", "paper", "scisssors"].includes(userChoice)) {
+      alert("Inavalid choice. Please choose rock, paper or scissors.");
+      return getUserChoice();
+    }
+  
+    // return userChoice
+    return userChoice;
+  }
+  
