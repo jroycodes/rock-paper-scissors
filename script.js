564bd64ee59d6cac00df1d03fd6202c98ceb2fea
Jonathan Roy <jonaisroy@gmail.com>
2024-04-15 11:41:44 +0300

    updated functions [getComputerChoice, getUserChoice]

--- script.js
+++ script.js
@@ -1,33 +1,36 @@
 "use strict";
 
-// Get comoputer choice
+ let computerChoice;
+
+// Get computer choice
 function getComputerChoice() {
   // choices
-  const choices = ["rock", "paper", "scissors"];
-
+ const choices = ["rock", "paper", "scissors"];
   // get random indexes
   const randomIndex = Math.floor(Math.random() * choices.length);
   console.log(choices);
 
   // return choice corresponding to random index
-  return choices[randomIndex];
+  computerChoice = choices[randomIndex];
+  return computerChoice;
 }
-console.log(getComputerChoice());
+getComputerChoice();
+console.log(computerChoice);
 
+let userChoice;
+// Get user input 
+function getUserChoice(){
+  // prompt for user input 
+userChoice = prompt("Rock, Paper, Scissors?");
 
-// Get user choice
-function getUserChoice() {
-  // prompt for user input
-  let userChoice = prompt("Rock, Paper, Scissors?");
-
-  // Validate user input
+  // validate user input 
   userChoice = userChoice.toLowerCase();
   if (!["rock", "paper", "scissors"].includes(userChoice)) {
-    alert("Inavalid choice. Please choose rock, paper or scissors.");
-    return getUserChoice();
-  }
+        alert("Inavalid choice. Please choose rock, paper or scissors.");
+        return getUserChoice();
+      }
 
-  // return userChoice
-  return userChoice;
+    return userChoice;
 }
-console.log(getUserChoice());
+getUserChoice();
+console.log(userChoice);
